019ecd784a205a0da29507b854aa423af6a95c6c
liongeron <80258340+liongeron@users.noreply.github.com>
2022-03-01 14:15:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 app.use(express.json())
 app.use(express.static('public'));
 
